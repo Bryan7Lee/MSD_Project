ca2901345b53fafe290907429eb39f09cd7e5c21
Bryan Lee <seojunleeb@gmail.com>
2026-03-15 22:33:42 -0400

    fixing repo

--- device_firmware/main.cpp
+++ device_firmware/main.cpp
@@ -1,22 +1,39 @@
-// libraries
+// ---- Libraries ----
 #include <stdio.h>
 #include "pico/stdlib.h" 
 #include "hardware/adc.h"
 #include "tusb.h"
 #include <map>
 
-// DEFINES
+// ---- DEFINES ----
 #define LED_PIN 25     // built-in LED on board
-#define BUTTON1_PIN 13 // left click button
-#define BUTTON2_PIN 15 // right click button
+#define SS_BUTTON_PIN 14 // scrolling speed cycle button
+#define LC_BUTTON_PIN 13 // left click button
+#define RC_BUTTON_PIN 15 // right click button
 #define LEFTCLICK_MASK 1 
 #define RIGHTCLICK_MASK 2
 
+// ---- Structs and whatnot ----
 // Current button mappings (example: map to keyboard keys)
 std::map<int, uint8_t> button_mapping = {
-    {BUTTON1_PIN, HID_KEY_T},   // default: T key
-    {BUTTON2_PIN, HID_KEY_Y}   // default: Y key
+    {LC_BUTTON_PIN, HID_KEY_T},   // default: T key
+    {RC_BUTTON_PIN, HID_KEY_Y}   // default: Y key
 };
+
+struct ScrollSpeed
+{
+    uint32_t interval_us;          // how often scroll ticks send
+    uint16_t sensitivity_divider;  // joystick scaling 
+};
+
+// 3 default profiles
+ScrollSpeed scrollSpeeds[3] = {
+    {8000, 2000},   // Profile 0 (fast)
+    {15000, 2500},  // Profile 1 (medium)
+    {30000, 3000}   // Profile 2 (slow)
+};
+
+uint8_t activeScrollSpeed = 0;
 
 // HID descriptor
 extern "C" uint16_t tud_hid_get_report_cb(uint8_t instance,
@@ -46,13 +63,18 @@ int main()
     static absolute_time_t click_time;
     absolute_time_t last_scroll_time = get_absolute_time();
     const uint32_t SCROLL_INTERVAL_US = 90000; // 90ms 
+    const uint32_t SCROLL_INTERVAL_X = 100000; // 100ms
     bool leftclick_pressed;
     bool rightclick_pressed;
     uint8_t buttons;
-    uint16_t raw;
-    int16_t centered;
-    int16_t scaled;
-    int8_t scroll;
+    uint16_t rawX;
+    uint16_t rawY;
+    int16_t centeredX;
+    int16_t centeredY;
+    int16_t scaledX;
+    int16_t scaledY;
+    int8_t scrollX;
+    int8_t scrollY;
 
     // ------ Inits ------
     stdio_init_all(); // init serial comms + board
@@ -63,14 +85,18 @@ int main()
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    // Mouse Buttons
-    gpio_init(BUTTON1_PIN);
-    gpio_set_dir(BUTTON1_PIN, GPIO_IN);
-    gpio_pull_up(BUTTON1_PIN);
+    // On board buttons
+    gpio_init(SS_BUTTON_PIN);
+    gpio_set_dir(SS_BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(SS_BUTTON_PIN);
 
-    gpio_init(BUTTON2_PIN); 
-    gpio_set_dir(BUTTON2_PIN, GPIO_IN);
-    gpio_pull_up(BUTTON2_PIN);
+    gpio_init(LC_BUTTON_PIN);
+    gpio_set_dir(LC_BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(LC_BUTTON_PIN);
+
+    gpio_init(RC_BUTTON_PIN); 
+    gpio_set_dir(RC_BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(RC_BUTTON_PIN);
 
     // Macro Buttons
 
@@ -80,56 +106,75 @@ int main()
         tud_task(); // must be called frequently as this is the USB hid task
 
         // ---- Button Press ----
-        leftclick_pressed = !gpio_get(BUTTON1_PIN);
-        rightclick_pressed = !gpio_get(BUTTON2_PIN);
+        leftclick_pressed = !gpio_get(LC_BUTTON_PIN);
+        rightclick_pressed = !gpio_get(RC_BUTTON_PIN);
 
         buttons = 0;
 
         if (leftclick_pressed)  buttons |= 0x01;
         if (rightclick_pressed) buttons |= 0x02;
 
-        // ---- Scrolling (GP27/ADC1) ----
-        adc_select_input(1);
-        raw = adc_read();  
+        // ---- Read Scrolling ----
+        // read horizontal (X axis) scrolling (pin GP26/ADC0)
+        adc_select_input(0);
+        rawX = adc_read();  
+        centeredX = (int16_t)rawX - 2048;
 
-        centered = (int16_t)raw - 2048;
+        // read vertical (Y axis) scrolling (pin GP27/ADC1)
+        adc_select_input(1);
+        rawY = adc_read();  
+        centeredY = (int16_t)rawY - 2048;
 
         // Deadzone to prevent POTENTIAL stick drift
-        if (centered > -10 && centered < 10) 
+        if (centeredY > -10 && centeredY < 10) 
         {
-            scroll = 0;
+            scrollY = 0;
         } 
         else 
         {
-            scaled = centered / 1500;  // linear scrolling w/ no acceleration
+            scaledY = centeredY / 1500;  // linear scrolling w/ no acceleration
 
             // Clamp
-            if (scaled > 1)  scaled = 1;
-            if (scaled < -1) scaled = -1;
+            if (scaledY > 1)  scaledY = 1;
+            if (scaledY < -1) scaledY = -1;
 
-            scroll = (int8_t)scaled;
+            scrollY = (int8_t)scaledY;
+        }
+
+        // Deadzone to prevent POTENTIAL stick drift
+        if (centeredX > -10 && centeredX < 10) 
+        {
+            scrollX = 0;
+        } 
+        else 
+        {
+            scaledX = centeredX / 1500;  // linear scrolling w/ no acceleration
+
+            // Clamp
+            if (scaledX > 1)  scaledX = 1;
+            if (scaledX < -1) scaledX = -1;
+
+            scrollX = -(int8_t)scaledX;
         }
 
         // ---- sending device reports ----
         if (tud_hid_ready())
-    {
+        {
         absolute_time_t now = get_absolute_time();
 
-        if (scroll != 0)
-        {
-            if (absolute_time_diff_us(last_scroll_time, now) > SCROLL_INTERVAL_US)
+            if (scrollX != 0 || scrollY != 0)
             {
-                tud_hid_mouse_report(0, buttons, 0, 0, scroll, 0);
-                last_scroll_time = now;
+                if (absolute_time_diff_us(last_scroll_time, now) > SCROLL_INTERVAL_US)
+                {
+                    tud_hid_mouse_report(0, buttons, 0, 0, scrollY, scrollX);
+                    last_scroll_time = now;
+                }
+            }
+            else
+            {
+                // Always send zero to stop scrolling immediately
+                tud_hid_mouse_report(0, buttons, 0, 0, 0, 0);
             }
         }
-        else
-        {
-            // Always send zero to stop scrolling immediately
-            tud_hid_mouse_report(0, buttons, 0, 0, 0, 0);
-        }
-    }
-
-        gpio_put(LED_PIN, leftclick_pressed);
     }
 }
